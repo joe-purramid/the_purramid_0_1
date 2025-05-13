4f1068cd1b9286e00d930d572bdb725cbe5d32a6
Joe Selby <93053243+joe-purramid@users.noreply.github.com>
2025-05-13 19:14:50 -0400

    Screen Shade Preview Launch debugging

--- app/src/main/kotlin/com/example/purramid/thepurramid/data/db/SpinListEntity.kt
+++ app/src/main/kotlin/com/example/purramid/thepurramid/data/db/SpinListEntity.kt
@@ -1,10 +1,12 @@
 // SpinListEntity.kt
 package com.example.purramid.thepurramid.data.db
 
+import androidx.room.Entity
 import java.util.UUID
 
+@Entity(tableName = "spin_lists")
 data class SpinListEntity(
     val id: UUID = UUID.randomUUID(), // Unique ID for each list
     var title: String,
-    val items: MutableList<SpinItem> = mutableListOf()
+    val items: MutableList<SpinItemEntity> = mutableListOf()
 )
