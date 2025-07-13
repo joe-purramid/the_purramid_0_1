e3d33e77038a4605b24c49d0823aaa7ba56bab73
Joe Selby <93053243+joe-purramid@users.noreply.github.com>
2025-07-13 01:45:43 -0400

    String concatenation

--- app/src/main/kotlin/com/example/purramid/thepurramid/AboutActivity.kt
+++ app/src/main/kotlin/com/example/purramid/thepurramid/AboutActivity.kt
@@ -1,23 +1,44 @@
 package com.example.purramid.thepurramid
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class AboutActivity : AppCompatActivity() {
 
-    val eulaText = getString(R.string.eula_content_part1) +
-            getString(R.string.eula_content_part2) +
-            getString(R.string.eula_content_part3) +
-            getString(R.string.eula_content_part4) +
-            getString(R.string.eula_content_part5) +
-            getString(R.string.eula_content_part6) +
-            getString(R.string.eula_content_part7) +
-            getString(R.string.eula_content_part8) +
-            getString(R.string.eula_content_part9) +
-            getString(R.string.eula_content_part10)
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_about)
+
+        val eulaText = getString(R.string.eula_content_part1) +
+                getString(R.string.eula_content_part2) +
+                getString(R.string.eula_content_part3) +
+                getString(R.string.eula_content_part4) +
+                getString(R.string.eula_content_part5) +
+                getString(R.string.eula_content_part6) +
+                getString(R.string.eula_content_part7) +
+                getString(R.string.eula_content_part8) +
+                getString(R.string.eula_content_part9) +
+                getString(R.string.eula_content_part10)
+
+        findViewById<TextView>(R.id.eula_text).text = eulaText
+
+        val balsamiqLicenseText = getString(R.string.balsamiq_sans_license_text_part1) +
+                getString(R.string.balsamiq_sans_license_text_part2)
+
+        findViewById<TextView>(R.id.balsamiq_sans_license).text = balsamiqLicenseText
+
+        val bestPracticesText = getString(R.string.best_practices_content_part1) +
+                getString(R.string.best_practices_content_part2) +
+                getString(R.string.best_practices_content_part3) +
+                getString(R.string.best_practices_content_part4) +
+                getString(R.string.best_practices_content_part5)
+
+        findViewById<TextView>(R.id.best_practices_text).text = bestPracticesText
+
+        val acknowledgementsText = getString(R.string.acknowledgements_content_part1) +
+                getString(R.string.acknowledgements_content_part2)
+
+        findViewById<TextView>(R.id.acknowledgements_text).text = acknowledgementsText
     }
 }
